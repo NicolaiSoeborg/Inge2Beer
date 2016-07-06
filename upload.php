f2e4eb2073d47909776ed435bd9f7676fa387a8d
Nicolai Søborg <NicolaiSoeborg+github@gmail.com>
2016-07-06 17:33:16 -0400

    Return zip containing everything

--- upload.php
+++ upload.php
@@ -3,27 +3,26 @@ if ((!empty($_FILES["rusliste"])) && ($_FILES["rusliste"]['error'] == 0)) {
   $filename = basename($_FILES["rusliste"]['name']);
   $ext = substr($filename, strrpos($filename, '.') + 1);
 
-  /*echo "FILENAME: ".$filename."<br>";
-  echo "EXT: ".$ext."<br>";
-  echo "SIZE: ".$_FILES["rusliste"]["size"]."<br>";*/
-
   // Check file ext and size < 15 mb
   if (($ext == "xls") && ($_FILES["rusliste"]["size"] < 15000000)) {
 
+      // Warning: newname _MUST_ to be hashed to protect 'exec'-call below.
       $newname = '/tmp/uploads/'.md5($filename).'-'.time();
       //Check if the file with the same name is already exists on the server
       if (!file_exists($newname)) {
 
         //Attempt to move the uploaded file to it's new place
         if ((move_uploaded_file($_FILES["rusliste"]['tmp_name'], $newname))) {
-           exec("/tmp/Inge2db/inge2db.py ".$newname);
-           $newname .= ".db";
+           exec("/tmp/Inge2Beer/inge2beer.py ".$newname);
+           exec("zip beer.zip ".$newname".*");
+           $newname .= ".zip";
            header('Content-Type: application/octet-stream');
-           header('Content-Disposition: attachment; filename="beer.db"');
+           header('Content-Disposition: attachment; filename="beer.zip"');
            header('Expires: 0');
            header('Cache-Control: must-revalidate');
            header('Content-Length: ' . filesize($newname));
            readfile($newname);
+           unlink($newname);
            exit;
         } else {
            echo "Error: A problem occurred during file upload!";
