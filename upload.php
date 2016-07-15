1c694a61022521495f8b08c78247a2a22901fd0d
Nicolai Søborg <NicolaiSoeborg+github@gmail.com>
2016-07-15 20:29:09 +0200

    New 'Inge2Beer' path and better error handling

--- upload.php
+++ upload.php
@@ -6,14 +6,15 @@ if ((!empty($_FILES["rusliste"])) && ($_FILES["rusliste"]['error'] == 0)) {
   // Check file ext and size < 15 mb
   if (($ext == "xls") && ($_FILES["rusliste"]["size"] < 15000000)) {
 
+      if (!is_dir("/tmp/uploads")) mkdir("/tmp/uploads");
       // Warning: newname _MUST_ to be hashed to protect 'exec'-call below.
-      $newname = '/tmp/uploads/'.md5($filename).'-'.time();
+      $newname = "/tmp/uploads/".md5($filename).'-'.time();
       //Check if the file with the same name is already exists on the server
       if (!file_exists($newname)) {
 
         //Attempt to move the uploaded file to it's new place
         if ((move_uploaded_file($_FILES["rusliste"]['tmp_name'], $newname))) {
-           exec("/tmp/Inge2Beer/inge2beer.py ".$newname);
+           exec("/var/www/private/Inge2Beer/inge2beer.py ".$newname);
            exec("cd \"/tmp/uploads/\" && /usr/bin/timeout 5s /usr/bin/latex -interaction=nonstopmode \"".$newname.".tex\" 2> /dev/null");
            exec("cd \"/tmp/uploads/\" && /usr/bin/timeout 5s /usr/bin/dvips \"".$newname.".dvi\" 2> /dev/null");
            exec("cd \"/tmp/uploads/\" && /usr/bin/timeout 5s /usr/bin/ps2pdf \"".$newname.".ps\" 2> /dev/null");
@@ -24,7 +25,7 @@ if ((!empty($_FILES["rusliste"])) && ($_FILES["rusliste"]['error'] == 0)) {
            $z = new ZipArchive();
            if (!$z->open($newname.".zip", ZipArchive::CREATE))
              die("Can't create zip file.");
-           $z->addFile("/tmp/Inge2Beer/templates/readme.txt", "README.txt");
+           $z->addFile("/var/www/private/Inge2Beer/templates/readme.txt", "README.txt");
            $z->addFile($newname.".pdf", "Barcodes.pdf");
            $z->addFile($newname.".tex", "Barcodes.tex");
            $z->addFile($newname.".db", "Beer.db");
